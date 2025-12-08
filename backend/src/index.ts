b9067a8f49dbdc3c82ce00651ff4ce76f2856d63
ranjit <ranjitdas2048@gmail.com>
2025-12-08 22:33:51 +0530

    added backend_1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript!");
+import { WebSocket, WebSocketServer } from "ws";
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection',(ws)=>{
+    ws.on('error', console.error);
+    
+    ws.on('message', (data)=>{
+        console.log(`receivd data from ${data}`);
+    });
+    ws.send('Connected')
+})
