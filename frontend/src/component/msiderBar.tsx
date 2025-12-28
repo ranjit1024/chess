9b503990b15b4d3f5be41ce582fe3bca2c83f073
ranjit <ranjitdas2048@gmail.com>
2025-12-28 23:03:40 +0530

    added responsivenes

--- frontend/src/component/msiderBar.tsx
+++ frontend/src/component/msiderBar.tsx
@@ -1,16 +1,20 @@
-export function MBmoves(){
+import { AlignVerticalDistributeStart, CameraIcon } from "lucide-react";
+
+export function MBmoves() {
     return <div className="bg-white/5 justify-between p-1 flex h-fit">
         <div>
             fsdf
         </div>
-        <div className="bg-white w-fit h-full">
-            
-        <video 
+        <div className="bg-black relative w-fit h-full">
+            <div className="absolute w-full bg-white/10 text-white flex justify-between items-center inset-0 blur  ">
+           
+            </div>
+            <video
                 autoPlay
                 playsInline
                 muted
                 className="h-29 w-fit overflow-hidden"
-              />
+            />
 
         </div>
     </div>
